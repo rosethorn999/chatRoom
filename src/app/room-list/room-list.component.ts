b22273afd63fbf3dea3d6fb7bdcd6c47d72d5d04
dbqp9876 <dbqp9876@gmail.com>
2017-05-14 20:41:12 +0900

    room list css and websocket test

--- src/app/room-list/room-list.component.ts
+++ src/app/room-list/room-list.component.ts
@@ -6,10 +6,54 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./room-list.component.css']
 })
 export class RoomListComponent implements OnInit {
+  wsUrl = "ws://jsonbin.org/lioufongkia/test";
+  websocket;
+  room_list = ['user1', 'user2'];
 
   constructor() { }
 
   ngOnInit() {
+    let headers = [];
+    headers[0] = "Authorization", "token e482852d-34f2-448b-a073-640574badeb3";
+    headers[1] = "Content-Type", "application/json";
+    this.websocket = new WebSocket(this.wsUrl,headers);
+    this.websocket.onopen = function (evt) {
+      this.onOpen(evt)
+    };
+    this.websocket.onclose = function (evt) {
+      this.onClose(evt)
+    };
+    this.websocket.onmessage = function (evt) {
+      this.onMessage(evt)
+    };
+    this.websocket.onerror = function (evt) {
+      this.onError(evt)
+    };
+  }
+
+  setRoomList() {
+
+  }
+
+  changeRoom(index: any) {
+    console.log(index);
+  }
+
+  onOpen(evt) {
+    console.log("CONNECTED"); 
+  }
+
+  onMessage(evt){
+    console.log("onmessage" + evt.data);
+    this.websocket.close();
+  }
+
+  onClose(evt) {
+    console.log("DISCONNECTED"); 
+  }
+
+  onError(evt) {
+    console.log("ERROR" + evt.data);     
   }
 
 }
